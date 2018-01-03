bf6a1c9ae4a96ff0d5fb2e604fd3e50c24569ba1
missmakita <mfernanlav@gmail.com>
2018-01-03 15:31:15 -0300

    Incorpora Firebase

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDEU_jVdKjoBNWIT1uiiO1wY4W_EU2vnNY",
+    authDomain: "startapp-5ea72.firebaseapp.com",
+    databaseURL: "https://startapp-5ea72.firebaseio.com",
+    projectId: "startapp-5ea72",
+    storageBucket: "startapp-5ea72.appspot.com",
+    messagingSenderId: "331023785710"
+  };
+  firebase.initializeApp(config);
