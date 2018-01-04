53d541492e94b68394cbfb959b30dd9b791c8f97
cubillosarteaga <cubilloslia@gmail.com>
2018-01-04 18:07:22 -0300

    cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
+$(document).ready(function(){
+  $(function(){
+   setTimeout(function() {
+      $('#splash').fadeOut(700);
+   }, 3000);
+});
+});
+
+
+
 $(document).ready(function() {
 
-	// Initialize Firebase
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDEU_jVdKjoBNWIT1uiiO1wY4W_EU2vnNY",
     authDomain: "startapp-5ea72.firebaseapp.com",
@@ -10,28 +20,32 @@ $(document).ready(function() {
     messagingSenderId: "331023785710"
   };
   firebase.initializeApp(config);
-	
-	// Enviar comentario y generar feed:
-	$('#sendBtn').click(function() {
-		if ($('#comment').val() !== "") {
-			$('.feed').append(
-				"<div class='feedBox list-group'>" +
+
+  
+  // Enviar comentario y generar feed:
+  $('#sendBtn').click(function() {
+    console.log("Entro")
+    var comment = $('#comment').val();
+    console.log(comment)
+    if(comment !== "") {
+      $('.feed').prepend(
+        "<div class='feedBox list-group'>" +
           "<a href='#' class='list-group-item active'>" +
             "<span>Usuario " + fullDate + "</span>" +
             "<h4 class='list-group-item-heading'>List group item heading</h4>" +
-            "<p class='list-group-item-text'>" + $('#comment').val() + "</p>" +
+            "<p class='list-group-item-text'>" + comment + "</p>" +
           "</a>" +
-        "</div>"	
-			);
-		}
-	});
-			
-	// Da formato a la fecha:
-		var todayFeed = new Date();
-		var date = todayFeed.getDate();
-		var month = todayFeed.getMonth();
-		var year = todayFeed.getFullYear();
-		var hours = todayFeed.getHours();
-		var minutes = todayFeed.getMinutes();
-		var fullDate = date +'/'+ month +'/'+ year +' '+ hours +':'+ minutes;
+        "</div>"  
+      );
+    }
+  });
+      
+  // Da formato a la fecha:
+    var todayFeed = new Date();
+    var date = todayFeed.getDate();
+    var month = todayFeed.getMonth();
+    var year = todayFeed.getFullYear();
+    var hours = todayFeed.getHours();
+    var minutes = todayFeed.getMinutes();
+    var fullDate = date +'/'+ month +'/'+ year +' '+ hours +':'+ minutes;
 });
