325165946839a283d3b235fe63f99d5432c04291
cubillosarteaga <cubilloslia@gmail.com>
2018-01-05 07:59:32 -0300

    modal amigos

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,9 +34,9 @@ function watcher() {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in:
-      $('.homepage').hide();
+     /* $('.homepage').hide();
       $('#header').show();
-      $('#first').show();
+      $('#first').show();*/
       console.log(user);
       var displayName = user.displayName;
       var email = user.email;
@@ -48,9 +48,9 @@ function watcher() {
       // ...
     } else {
       // User is signed out:
-      $('.homepage').show();
+      /*$('.homepage').show();
       $('#header').hide();
-      $('#first').hide();  
+      $('#first').hide();  */
       // ...
     }
   });
